1571e3246ab84528b4ca3a68ca97f479a589b47e
Chofa's <sofigmez9@gmail.com>
2025-06-16 21:27:10 -0400

    Se crean las entitys de ms-grades

---
+++ ms-grades/src/main/java/com/edutech/grades/entity/CourseQuiz.java
@@ -0,0 +1,43 @@
+package com.edutech.grades.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "course_quiz")
+public class CourseQuiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "course_id", nullable = false)
+    private Integer courseId;
+
+    @Size(max = 200)
+    @NotNull
+    @Column(name = "title", nullable = false, length = 200)
+    private String title;
+
+    @Size(max = 800)
+    @Column(name = "description", length = 800)
+    private String description;
+
+    @Size(max = 50)
+    @Column(name = "quiz_type", length = 50)
+    private String quizType;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+}
